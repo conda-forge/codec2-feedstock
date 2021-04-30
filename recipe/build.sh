16deb3abce9347eae064ab5dcab67b2e15884af6
Ryan Volz <ryan.volz@gmail.com>
2021-04-30 13:16:00 -0400

    Revert "Try cmake with a toolchain file for fixing osx-arm64 build."
    This reverts commit 379c82aaf1800000d9fe798846b4314c36410e45.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,18 +5,13 @@ set -ex
 mkdir build
 cd build
 
-# translate compiler activation script's CMAKE_ARGS to toolchain file
-# (this is so an in-project native build can work by not inheriting the args)
-eval echo "$CMAKE_ARGS" | sed -e 's/-D/set(/g;s/ /)\'$'\n''/g;s/=/ /g;$ s/$/)/g' > host_toolchain.cmake
-
 cmake_config_args=(
-    -DCMAKE_TOOLCHAIN_FILE=host_toolchain.cmake
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_PREFIX=$PREFIX
     -DCMAKE_INSTALL_LIBDIR=lib
     -DBUILD_SHARED_LIBS=ON
 )
 
-cmake .. "${cmake_config_args[@]}"
+cmake ${CMAKE_ARGS} .. "${cmake_config_args[@]}"
 cmake --build . --config Release -- -j${CPU_COUNT}
 cmake --build . --config Release --target install
