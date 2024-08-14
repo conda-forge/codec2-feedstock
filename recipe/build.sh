8dcb8d16388ee1ca7d190617f3f4ebea5437472d
Ryan Volz <ryan.volz@gmail.com>
2024-08-14 18:47:14 -0400

    Try this

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,10 +9,21 @@ if [[ "$target_platform" == "win-64" ]]; then
   export PREFIX=${PREFIX}/Library
 fi
 
+cmake_config_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_PREFIX_PATH=$PREFIX
+    -DBUILD_SHARED_LIBS=ON
+    -DCMAKE_C_COMPILER=$CC
+    -DCMAKE_CXX_COMPILER=$CXX
+    -DCMAKE_C_FLAGS="$CFLAGS"
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS"
+)
+
 # clear variables that are not necessary thanks to CMAKE_ARGS but prevent
 # in-tree native build of generate_codebook (picked up by CMAKE without flags)
-# but preserve CC and CXX first as a CMAKE arg
-CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_FLAGS='${CFLAGS}' -DCMAKE_CXX_FLAGS='${CXXFLAGS}'"
+# after having preserved CC and CXX first as a CMAKE args above
 unset AR
 unset CFLAGS
 unset CXXFLAGS
@@ -25,14 +36,6 @@ unset STRIP
 # because we set CMAKE_C(XX)_COMPILER
 CC="$CC_FOR_BUILD"
 CXX="$CXX_FOR_BUILD"
-
-cmake_config_args=(
-    -DCMAKE_BUILD_TYPE=Release
-    -DCMAKE_INSTALL_PREFIX=$PREFIX
-    -DCMAKE_INSTALL_LIBDIR=lib
-    -DCMAKE_PREFIX_PATH=$PREFIX
-    -DBUILD_SHARED_LIBS=ON
-)
 
 if [[ "$target_platform" == "win-64" ]]; then
     cmake_config_args+=(
