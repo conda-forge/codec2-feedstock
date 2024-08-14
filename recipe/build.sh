2008337915a07449e7a517e92b888b926c0cc920
Ryan Volz <ryan.volz@gmail.com>
2024-08-14 17:49:55 -0400

    Try another

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ fi
 # clear variables that are not necessary thanks to CMAKE_ARGS but prevent
 # in-tree native build of generate_codebook (picked up by CMAKE without flags)
 # but preserve CC and CXX first as a CMAKE arg
-CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_FLAGS="\'"${CFLAGS}"\'" -DCMAKE_CXX_FLAGS="\'"${CXXFLAGS}"\'
+CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_FLAGS=\"${CFLAGS}\" -DCMAKE_CXX_FLAGS=\"${CXXFLAGS}\""
 unset AR
 unset CFLAGS
 unset CXXFLAGS
@@ -40,5 +40,5 @@ if [[ "$target_platform" == "win-64" ]]; then
     )
 fi
 
-cmake -G "Ninja" ${CMAKE_ARGS} .. "${cmake_config_args[@]}"
+cmake -G "Ninja" "${CMAKE_ARGS}" .. "${cmake_config_args[@]}"
 cmake --build . --config Release -- -j${CPU_COUNT}
