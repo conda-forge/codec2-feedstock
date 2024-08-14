5b0c741badc8014b425c72e51f2f7c9ddde280f8
Ryan Volz <ryan.volz@gmail.com>
2024-08-14 16:01:56 -0400

    Try to get right compilers for in-tree native build of generate_codebook

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,14 +7,18 @@ cd build
 
 # clear variables that are not necessary thanks to CMAKE_ARGS but prevent
 # in-tree native build of generate_codebook (picked up by CMAKE without flags)
-# but preserve CC first as a CMAKE arg
-# CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_C_COMPILER=$CC"
-# unset AR
-# unset CC
-# unset LD
-# unset NM
-# unset RANLIB
-# unset STRIP
+# but preserve CC and CXX first as a CMAKE arg
+CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"
+unset AR
+unset LD
+unset NM
+unset RANLIB
+unset STRIP
+# set CC and CXX variables to build env compilers so in-tree native build of
+# generate_codebook picks up the right compilers (not used for regular build
+# because we set CMAKE_C(XX)_COMPILER
+CC="$CC_FOR_BUILD"
+CXX="$CXX_FOR_BUILD"
 
 cmake_config_args=(
     -DCMAKE_BUILD_TYPE=Release
